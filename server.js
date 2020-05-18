2dac7a5dcd8715f07e009399e77acb232ba3db77
supernovalx <hoanganhgo9@gmail.com>
2020-05-18 15:11:08 +0700

    fix node

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ var express = require('express');
 const path = require('path');
 var app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'dist/HRplatform-front-end'))); 
+
+var angularDist=__dirname+'/dist/HRplatform-front-end';
+app.use(express.static(angularDist));
+app.get('/', (req, res) =>  res.sendFile(path.join(andularDist+'/index.html')));
+
 app.listen(PORT,() => console.log(`Listening on ${ PORT }`));
